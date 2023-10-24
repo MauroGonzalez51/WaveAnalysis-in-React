15d4bdb14fc6334e278ee75dad64b268938253d7
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2023-10-23 20:18:12 -0500

    Deleting the RecordMedia Component

--- src/UploadFiles.jsx
+++ src/UploadFiles.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import DragDropFiles from "./DragDropFiles";
-// import RecordMedia from "./RecordMedia";
 
 const Container = styled.div`
     display: flex;
@@ -10,16 +9,13 @@ const Container = styled.div`
     width: 450px;
     aspect-ratio: 16 / 9;
     border: 2px dashed #c3c3c3;
-    font-size: .8rem;
+    font-size: 0.8rem;
 `;
 
 function UploadFiles({ handleOnChange }) {
     return (
         <Container>
-            <div>
-                <DragDropFiles handleOnChange={handleOnChange}/>
-                {/* <RecordMedia /> */}
-            </div>
+            <DragDropFiles handleOnChange={handleOnChange} />
         </Container>
     );
 }
